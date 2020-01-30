48e3c49edaa4eef713fa097b62e88b211d1059ff
Richard Ram <richard@richard-ram.com>
2020-01-30 16:36:00 +0100

    Basic implementation of 2 display and camera support.

---
+++ Assets/Scripts/ActivateAllDisplays.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Eurovision.Viewports
+{
+    // Working in Unity 2019.2 on OSX?
+    // 1. Make sure you unselect auto graphics api in the player settings
+    // 2. Drag and drop the metal down
+    public class ActivateAllDisplays : MonoBehaviour
+    {
+        void Start ()
+        {
+            // Display.displays[0] is the primary, default display and is always ON, so start at index 1.
+            // Check if additional displays are available and activate each.
+            
+            for (int i = 0; i < Display.displays.Length; i++)
+            {
+                Display.displays[i].Activate(1024, 768, 60);
+            }
+        }
+    }
+}
