01c1f742707e9ebe4e47a273f1371c28d15e7266
DragstylerNL <dragstylernl@gmail.com>
2020-03-24 15:38:05 +0100

    Update on Particles in main scene

---
+++ Assets/Scripts/FollowObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    // ============================================================================================= "private" variables
+    [SerializeField] private Transform followObject;
+    [SerializeField] private Vector3 offset;
+
+    // ========================================================================================================== Update
+    void Update()
+    {
+        transform.position = followObject.position + offset;
+    }
+}
