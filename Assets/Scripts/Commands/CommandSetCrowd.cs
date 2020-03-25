b089fce0bc851f07d6f8128a251ba487310a4f4b
blizzard190 <22256@ma-web.nl>
2020-03-25 17:10:40 +0100

    Finnished crowd behavior with random controller and random offset in animation

--- Assets/Scripts/Commands/CommandSetCrowd.cs
+++ Assets/Scripts/Commands/CommandSetCrowd.cs
@@ -2,16 +2,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
+using UnityEditor.Animations;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CommandSetCrowd : Command<BehaviorIds, CrowdBehaviorDTO>
 {
     public override BehaviorIds Id => BehaviorIds.Crowd;
+    public float offset;
+    
+    private AnimatorOverrideController[] _setAnimators;
     private Animator[] _animators;
+    private AnimatorStateInfo _current;
     
     private void Start()
     {
+        _setAnimators = Resources.LoadAll<AnimatorOverrideController>("Animators");
         _animators = this.GetComponentsInChildren<Animator>();
+        _current = _animators[1].GetCurrentAnimatorStateInfo(0);
+        for (int i = 0; i < _animators.Length; i++)
+        {
+            _animators[i].runtimeAnimatorController = _setAnimators[Random.Range(0,3)];
+            _animators[i].Play(_animators[i].GetCurrentAnimatorStateInfo(0).shortNameHash, 0, Random.Range(0f,offset));
+        }
+        //Debug.Log(_setAnimators.Length + "Hello");
     }
 
     public override void Execute(CrowdBehaviorDTO commadData)
@@ -19,6 +33,15 @@ public class CommandSetCrowd : Command<BehaviorIds, CrowdBehaviorDTO>
         for (int i = 0; i < _animators.Length; i++)
         {
             _animators[i].SetFloat("Hype", commadData.crowdHype);
+           // _animators[i].SetFloat("Offset",Random.Range(0f, offset));
         }
+        if (_current.shortNameHash != _animators[1].GetCurrentAnimatorStateInfo(0).shortNameHash)
+        {
+            for (int i = 0; i < _animators.Length; i++)
+            {
+                _animators[i].Play(_animators[i].GetCurrentAnimatorStateInfo(0).shortNameHash, 0, Random.Range(0f,offset));
+            }
+        }
+        _current = _animators[1].GetCurrentAnimatorStateInfo(0);
     }
 }
