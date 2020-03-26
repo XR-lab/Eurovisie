77d20352e8b396cb3b33f00fc3177d7682d58dac
DragstylerNL <dragstylernl@gmail.com>
2020-03-26 11:15:00 +0100

    Revert "Revert "Merge pull request #12 from XR-lab/feature/Crowd-Behavior""
    This reverts commit ae15eb3c8a7e6867ee885b4682c2fbe066038a21.

---
+++ Assets/Scripts/Commands/Commander.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Commander<TCommandID, TCommandDataType> : MonoBehaviour
+{
+    private readonly Dictionary<TCommandID, Command<TCommandID,TCommandDataType>> commands = new Dictionary<TCommandID, Command<TCommandID,TCommandDataType>>();
+
+    public void Apply(TCommandID commandId, TCommandDataType commandData)
+    {
+        if(!commands.ContainsKey((commandId)))
+            return;
+
+        commands[commandId].Execute(commandData);
+    }
+
+    public void AddCommand(TCommandID commandId, Command<TCommandID, TCommandDataType> newCommand)
+    {
+        commands.Add(commandId,newCommand);
+        newCommand.Init();
+    }
+
+    public void RemoveCommand(TCommandID commandId)
+    {
+        commands.Remove(commandId);
+    }
+}
