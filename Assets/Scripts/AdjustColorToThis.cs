83aa7e3210c8d4035000b808c43c60a0dbb41f26
DragstylerNL <dragstylernl@gmail.com>
2020-03-30 14:28:33 +0200

    advanced Lighting

---
+++ Assets/Scripts/AdjustColorToThis.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustColorToThis : MonoBehaviour
+{
+	// =================================================================================== variables
+	[Serialisablefield] private GameObject[] objects;
+	private Material[] mats;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
