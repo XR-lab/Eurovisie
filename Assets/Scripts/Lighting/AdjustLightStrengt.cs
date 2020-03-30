5f59cde83ca90b16b113b661b1815daa13fb8ec7
DragstylerNL <dragstylernl@gmail.com>
2020-03-30 17:22:24 +0200

    SceneLighingDone

---
+++ Assets/Scripts/Lighting/AdjustLightStrengt.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustLightStrengt : MonoBehaviour
+{
+    // ==============================================================================================='public' variables
+    [SerializeField] private Light[] lights;
+    [SerializeField] private float middleGround;
+    [SerializeField] private float multiplier;
+
+    // =============================================================================================== private variables
+    private float[] _lightBaseStrenght;
+
+    private void Awake()
+    {
+        _lightBaseStrenght = new float[lights.Length];
+        for (int i = 0; i < lights.Length; i++)
+        {
+            _lightBaseStrenght[i] = lights[i].intensity;
+        }
+
+    }
+
+    //===================================================================================================== Change Value
+    public void ChangeLightValue(float strenght)
+    {
+        print(strenght);
+        float _strenght;
+        for (int i = 0; i < lights.Length; i++)
+        {
+            _strenght = ((strenght / middleGround) + 1) * _lightBaseStrenght[i];
+            lights[i].intensity = _strenght;
+        }
+    }
+}
