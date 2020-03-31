06c05a82da4b5f6f31ee8c75fc2804d4baff8af9
DragstylerNL <dragstylernl@gmail.com>
2020-03-31 14:30:00 +0200

    wack

--- Assets/Scripts/Lighting/AdjustLightStrengt.cs
+++ Assets/Scripts/Lighting/AdjustLightStrengt.cs
@@ -8,9 +8,12 @@ public class AdjustLightStrengt : MonoBehaviour
     [SerializeField] private Light[] lights;
     [SerializeField] private float middleGround;
     [SerializeField] private float multiplier;
+    [SerializeField] private Transform[] cones;
 
     // =============================================================================================== private variables
     private float[] _lightBaseStrenght;
+    private Vector3[] _baseConeScale;
+    
 
     private void Awake()
     {
@@ -20,6 +23,11 @@ public class AdjustLightStrengt : MonoBehaviour
             _lightBaseStrenght[i] = lights[i].intensity;
         }
 
+        _baseConeScale = new Vector3[cones.Length];
+        for (int i = 0; i < cones.Length; i++)
+        {
+            _baseConeScale[i] = cones[i].localScale;
+        }
     }
 
     //===================================================================================================== Change Value
@@ -29,8 +37,12 @@ public class AdjustLightStrengt : MonoBehaviour
         float _strenght;
         for (int i = 0; i < lights.Length; i++)
         {
-            _strenght = ((strenght / middleGround) + 1) * _lightBaseStrenght[i];
-            lights[i].intensity = _strenght;
+            _strenght = (strenght / middleGround) + 1;
+            lights[i].intensity = _strenght * _lightBaseStrenght[i];
+            
+            Vector3 _scale = ((_strenght/4) * _baseConeScale[i]) + _baseConeScale[i];
+            _scale.y = _baseConeScale[i].y;
+            cones[i].localScale = _scale;
         }
     }
 }
