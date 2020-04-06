ad8b1809d4bc87001b622875223dfcff85df0ca2
ColinvD <22950@ma-web.nl>
2020-04-06 10:00:18 +0200

    working moving lights for hanging lights

--- Assets/Scripts/Effects/LightsMover.cs
+++ Assets/Scripts/Effects/LightsMover.cs
@@ -2,15 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class LightsMover : MonoBehaviour
 {
     [SerializeField] private List<Transform> _lightTransforms;
+    [SerializeField] private List<Quaternion> _originRots;
     [SerializeField] private GameObject _lookpoint;
-    private float _originRot = 0f;
-    private float _maxDist = 40f;
-    private float _rotationStep = 2f;
+    [SerializeField] private float _originRot = 0f;
+    [SerializeField] private float _maxDist = 40f;
+    [SerializeField] private float _rotationStep = 2f;
+    [SerializeField] private float _heightOffset;
+    [SerializeField] private float _widthOffset;
     private event Action _movement;
+    private List<Action> _movements = new List<Action>();
+    private int index = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +31,14 @@ public class LightsMover : MonoBehaviour
             }
         }
 
-        _movement = MoveSideways;
+        for (int i = 0; i < _lightTransforms.Count; i++)
+        {
+            _originRots.Add(_lightTransforms[i].rotation);
+        }
+        
+        _movements.Add(MoveSideways);
+        _movements.Add(MoveBackAndForth);
+        _movement = _movements[index];
     }
 
     // Update is called once per frame
@@ -33,9 +47,20 @@ public class LightsMover : MonoBehaviour
         _movement();
     }
 
+    public void ChangeMovement()
+    {
+        for (int i = 0; i < _lightTransforms.Count; i++)
+        {
+            _lightTransforms[i].rotation = _originRots[i];
+        }
+        //_movement = _movements[Random.Range(0, _movements.Count)];
+        index++;
+        _movement = _movements[index%_movements.Count];
+    }
+
     private void MoveSideways()
     {
-        if (_originRot >= _maxDist || _originRot <= -_maxDist)
+        if (_originRot + _widthOffset >= _maxDist + _widthOffset || _originRot + _widthOffset <= -_maxDist + _widthOffset)
         {
             _rotationStep *= -1;
         }
@@ -45,12 +70,30 @@ public class LightsMover : MonoBehaviour
         for (int i = 0; i < _lightTransforms.Count; i++)
         {
             Vector3 rotation = _lightTransforms[i].rotation.eulerAngles;
-            rotation.x += _rotationStep;
-            rotation.y = -90;
-            //rotation.z = 90;
-            //_lightTransforms[i].Rotate(0,rotation.y, 0);
-            //_lightTransforms[i].rotation = Quaternion.Euler(-90, rotation.y, 0);
-            _lightTransforms[i].rotation = Quaternion.Euler(rotation);
+            rotation.y += _rotationStep;
+            //Debug.Log("Sideways = " + rotation.y);
+            //_lightTransforms[i].rotation = Quaternion.Euler(rotation);
+            _lightTransforms[i].rotation = Quaternion.Euler(new Vector3(rotation.x, _originRots[i].eulerAngles.y + _originRot + _widthOffset, rotation.z));
+        }
+    }
+    
+    private void MoveBackAndForth()
+    {
+        if (_originRot + _heightOffset >= _maxDist + _heightOffset || _originRot + _heightOffset <= -_maxDist + _heightOffset)
+        {
+            _rotationStep *= -1;
+        }
+        
+        //_originRot += _rotationStep;
+        
+        for (int i = 0; i < _lightTransforms.Count; i++)
+        {
+            Vector3 rotation = _lightTransforms[i].rotation.eulerAngles;
+            //rotation.x += _rotationStep;
+            Debug.Log("What I should have: " + (_originRots[i].x + _originRot) + ", what i get: " + rotation.x);
+            //Debug.Log("Back forth = " + rotation.x);
+            //_lightTransforms[i].rotation = Quaternion.Euler(rotation);
+            _lightTransforms[i].rotation = Quaternion.Euler(new Vector3(_originRots[i].x + _originRot + _heightOffset, rotation.y, rotation.z));
         }
     }
     
