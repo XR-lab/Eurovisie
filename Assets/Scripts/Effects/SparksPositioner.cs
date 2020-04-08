f2b0ac28bd74f88c291080d32ebab9150a7da4e0
DragstylerNL <dragstylernl@gmail.com>
2020-04-08 10:53:01 +0200

    some un neaded stuff, but shouldn't delete

---
+++ Assets/Scripts/Effects/SparksPositioner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SparksPositioner : MonoBehaviour
+{
+    // ======================================================================================================= Variables
+    [SerializeField] private Transform playerCam;
+    [SerializeField] private Transform uiCam;
+
+    private Vector3 _lastPos;
+    
+    // ========================================================================================================== Update
+    private void Update()
+    {
+        
+    }
+}
